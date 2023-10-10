d944aa9b4551b50578ae5d9d405382ace3d190d9
Manuel Francisco <ciesafrancisco@hotmail.com>
2023-10-10 14:32:07 +0200

    add Application file

--- src/Application.jsx
+++ src/Application.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 
 // Import Style
-import './styles/style.css'; // Assuming your CSS is in the 'styles' directory
+import './styles/style.css';  
 
 // Import Pages
 import Home from './pages/Home';
-import AboutKasa from './pages/Propos';
-import Rental from './pages/Logement'; // Make sure to match the case of the folder name
+import AboutUs from './pages/Propos';
+import Rental from './pages/Logement';  
 import Error from './components/Error';
 
 // Import Components
@@ -23,11 +23,11 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/logement/:rentalId' element={<Rental />} />
-          <Route path='/a-propos' element={<AboutKasa />} />
-          <Route path='/adresse-introuvable' element={<Error />} />
+          <Route path='/a-propos' element={<AboutUs />} />
+          <Route path='/adresse-unfindable' element={<Error />} />
           <Route
             path='*'
-            element={<Navigate to='/adresse-introuvable' replace />}
+            element={<Navigate to='/adresse-unfindable' replace />}
           />
         </Routes>
       </main>
